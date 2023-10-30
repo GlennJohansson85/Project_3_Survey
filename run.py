8d3f38091d9fca55bb47cba3ad6267bbb17433bc
GlennJo <139962883+GlennJohansson85@users.noreply.github.com>
2023-10-30 17:28:46 +0100

    Update run.py
    Add scope and creds path

--- run.py
+++ run.py
@@ -3,3 +3,10 @@ from google.oauth2.service_account import Credentials
 import pandas as pd
 from collections import Counter
 
+SCOPE = [
+  "https://www.googleapis.com/auth/spreadsheets",
+  "https://www.googleapis.com/auth/drive.file",
+  "https://www.googleapis.com/auth/drive",
+]
+
+CREDS_PATH = "creds.json"
