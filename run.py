29770ef374836dc14d2e0a0d85a8d45009cbe12a
GlennJo <139962883+GlennJohansson85@users.noreply.github.com>
2023-10-30 17:22:50 +0100

    Update run.py
    Import libraries

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
-Initial commit
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+import pandas as pd
+from collections import Counter
+
