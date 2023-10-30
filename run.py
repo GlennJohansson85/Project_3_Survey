1839123c9796f21d309d3cfa9f7885c08c81a9e9
GlennJohansson85 <139962883+GlennJohansson85@users.noreply.github.com>
2023-10-30 17:57:57 +0100

    Update run.py
    Authorize Google Sheets API:
    Implement the function to authorize the Google Sheets API using the provided Credentials

--- run.py
+++ run.py
@@ -10,3 +10,16 @@ SCOPE = [
 ]
 
 CREDS_PATH = "creds.json"
+
+def authorize_gspread():
+    """
+    Authorize the Google Sheets API using credentials.
+    Returns:
+        Authorized gspread client or None on error. 
+    """
+    try:
+        creds = Credentials.from_service_account_file(CREDS_PATH, scopes=SCOPE)
+        return gspread.authorize(creds)
+    except Exception as e:
+        print("Error authorizing gspread:", e)
+        return None
